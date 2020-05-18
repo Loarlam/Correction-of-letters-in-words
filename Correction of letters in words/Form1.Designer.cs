440c497929360265a5b819afc4cb04f48f8e1210
Loarlam <messiah02@mail.ru>
2020-05-18 11:56:18 +0300

    Добавлены gif и jpg файлы, в которых можно видеть работу приложения

--- Correction of letters in words/Form1.Designer.cs
+++ Correction of letters in words/Form1.Designer.cs
@@ -49,6 +49,7 @@
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox1.Location = new System.Drawing.Point(8, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
